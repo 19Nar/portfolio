d455c036921724c40f14a4867f34703850179eb1
narine <narinemkn@gmail.com>
2020-07-03 10:55:58 +0200

    Updates

---
+++ precache-manifest.c4799b6301a6d867c6347582e8fb23be.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0c8e9ec737a60b57832d8ea859862042",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "b7d5c02d1b2641c9b332",
+    "url": "/portfolio/static/css/main.79a9be6e.chunk.css"
+  },
+  {
+    "revision": "a0530544d15ea57676eb",
+    "url": "/portfolio/static/js/2.708a338b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/portfolio/static/js/2.708a338b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b7d5c02d1b2641c9b332",
+    "url": "/portfolio/static/js/main.30416262.chunk.js"
+  },
+  {
+    "revision": "7a6cc99bd8f08f9fa6ec",
+    "url": "/portfolio/static/js/runtime-main.209c2d7e.js"
+  },
+  {
+    "revision": "8d91835360804b1daa16efce88a83589",
+    "url": "/portfolio/static/media/GITHUB.8d918353.png"
+  },
+  {
+    "revision": "f3c65a2cd8165cf4935d66a95c664b61",
+    "url": "/portfolio/static/media/PRJcollage.f3c65a2c.jpg"
+  },
+  {
+    "revision": "80dc76174172613da9ac7c2ff646fda9",
+    "url": "/portfolio/static/media/book.80dc7617.png"
+  },
+  {
+    "revision": "519034148122a3b6263d6a1f8eedfb41",
+    "url": "/portfolio/static/media/facebook.51903414.png"
+  },
+  {
+    "revision": "377c4ca7896eb08695f8e94bf53bdd85",
+    "url": "/portfolio/static/media/linkedin.377c4ca7.jpg"
+  },
+  {
+    "revision": "addb3122b834566fcefd386d7bbd9ee7",
+    "url": "/portfolio/static/media/movie.addb3122.png"
+  },
+  {
+    "revision": "8f68715e0f8e1ccb2282a2406288b22a",
+    "url": "/portfolio/static/media/weather.8f68715e.png"
+  }
+]);
