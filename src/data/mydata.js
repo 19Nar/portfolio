02c9b78ba1915b1afe02632454b38d855d29c68f
Narine Vanbeginne <60644251+19Nar@users.noreply.github.com>
2020-07-03 13:52:33 +0200

    Update mydata.js

--- src/data/mydata.js
+++ src/data/mydata.js
@@ -37,7 +37,7 @@ export default {
       "#React.js   #MongoDB   #Express.js",
   ],
       imageSrc: Movie,
-      url: "/moviecms",
+      url: "http://www.narinevanbeginne.be/movie",
     },
     {
       id: 2,
